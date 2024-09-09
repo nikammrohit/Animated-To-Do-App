6f427e2c10b5f347746bab2f8b2cf06f4c65a37c
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-09 11:26:20 -0500

    Initial Commit

---
+++ AnimatedToDoApp/AnimatedToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedToDoAppApp.swift
+//  AnimatedToDoApp
+//
+//  Created by Rohit Nikam on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
