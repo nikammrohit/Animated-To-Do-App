c9aeec83a1bcab14ee76c182810156abec002b99
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-09 13:05:23 -0500

    Added strikethough feature for tasks when checkmark is pressed

--- AnimatedToDoApp/ContentView.swift
+++ AnimatedToDoApp/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isChecked: Bool = true
+    @State private var taskText: String = ""
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(red: 26/255, green: 27/255, blue: 37/255)
+                .edgesIgnoringSafeArea(.all)
+                
+            VStack{
+                //header image
+               Image("headerImage")
+                    .resizable()
+                    .scaledToFit()
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                HStack{
+                    //checkmark button
+                    Button(action: {
+                        isChecked.toggle()
+                    }) {
+                        Image(systemName: isChecked ? "checkmark.square" : "checkmark.square.fill")
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            .font(.title2)
+                            .position(x: 40, y:-20)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    
+                    Text(taskText.isEmpty ? "Task" : taskText)
+                            .foregroundColor(.white)
+                            .strikethrough(!isChecked, color: .white)
+                            .animation(.easeInOut, value: isChecked)
+                            .position(x: -35, y:-20)
+                    
+                    
+                    TextField("Task", text: $taskText)
+                        .foregroundColor(.white)
+                        .position(x: -35, y:-20)
+                        .opacity(0)
+                    
+                        
+                }
+                
+                //
+                
+            }
+            
         }
-        .padding()
     }
 }
 
